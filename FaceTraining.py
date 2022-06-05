fcfa25c30284094e6216bd15b140097d6c55e713
Malcom <43865589+MalcomMaynard77@users.noreply.github.com>
2022-06-05 18:33:16 -0400

    2nd commit

--- FaceTraining.py
+++ FaceTraining.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image
 import numpy as np
 import cv2
+import pickle
 
 trained_face_data = cv2.CascadeClassifier(
     'data\haarcascade_frontalface_alt.xml')
@@ -12,11 +13,12 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # grabbing the directory of the faces folder
 image_dir = os.path.join(BASE_DIR, "Faces")
 
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 current_id = 0
 label_ids = {}
 x_train = []
 y_labels = []
-print(image_dir)
+
 # see the files in the faces folder
 for root, dirs, files in os.walk(image_dir):
 
@@ -24,12 +26,12 @@ for root, dirs, files in os.walk(image_dir):
         # looking for files that end with png and jpg
         if file.endswith("png") or file.endswith("jpg") or file.endswith("HEIC"):
             path = os.path.join(root, file)
-            print(path)
-            """
+            # print(path)
+
             # creating a label for the images
             label = os.path.basename(os.path.dirname(
                 path)).replace(" ", "-").lower()
-            print(label, path)
+            #print(label, path)
 
             # adding new images to the dictionary and adding a id to them
             if not label in label_ids:
@@ -38,21 +40,29 @@ for root, dirs, files in os.walk(image_dir):
                 current_id += 1
 
             id_ = label_ids[label]
-            print(label_ids)
+            # print(label_ids)
 
             pil_image = Image.open(path).convert(
                 "L")  # turns images into grey scale
+            size = (550, 500)
+            finale_image = pil_image.resize(size, Image.ANTIALIAS)
             # adding the images into a SUPER array
-            image_array = np.array(pil_image, "uint8")
-            print(image_array)
+            image_array = np.array(finale_image, "uint8")
+            # print(image_array)
             faces = trained_face_data.detectMultiScale(
                 image_array, scaleFactor=1.5, minNeighbors=5)
-            # y_labels.append(label)
-            # x_train.append(path)
 
             for (x, y, w, h) in faces:
                 roi = image_array[y:y+h, x:x+w]
                 x_train.append(roi)
-                y_labels.append(id_)"""
-print(y_labels)
-print(x_train)
+                y_labels.append(id_)
+# print(y_labels)
+# print(x_train)
+
+# saving all the Label ID's
+with open("labels.pickle", 'wb') as f:
+    pickle.dump(label_ids, f)
+    f.close()
+# using data to train the recognizer and saving its data
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("trainner.yml")
--- Faces.py
+++ Faces.py
@@ -1,10 +1,24 @@
+from cProfile import label
 import numpy as np
 import cv2
 from scipy.misc import face
+import pickle
 
+recongizer = cv2.face.LBPHFaceRecognizer_create()
 face_cascade = cv2.CascadeClassifier(
     'data\haarcascade_frontalface_alt.xml')
 
+eye_cascade = cv2.CascadeClassifier(
+    'data\haarcascade_eye.xml')
+# reading the data
+recongizer.read("trainner.yml")
+
+# saving all the Label ID's to a dic
+labels = {}
+with open("labels.pickle", 'rb') as f:
+    labels = pickle.load(f)
+    labels = {v: k for k, v in labels.items()}
+    f.close()
 
 cap = cv2.VideoCapture(0)
 
@@ -16,9 +30,9 @@ while (True):
     faces = face_cascade.detectMultiScale(
         grey, scaleFactor=1.5, minNeighbors=4)
 
-    print(faces)
+    # print(faces)
     for (x, y, w, h) in faces:
-        print(x, y, w, h)
+        #print(x, y, w, h)
 
         # defining the ROI for the color and grey frame
         roi_grey = grey[y:y+h, x:x+w]
@@ -33,6 +47,20 @@ while (True):
         height = y+h
         cv2.rectangle(frame, (x, y), (width, height), color, stroke)
 
+        # finding eyes on the frame
+
+        # getting the label and the confidence value
+        id_, conf = recongizer.predict(roi_grey)
+
+        if conf >= 60 and conf <= 99:
+            print(id_)
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_PLAIN
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x, y), font, 1,
+                        color, stroke, cv2.LINE_AA)
     # show the frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(20) and 0xFF == ord('q'):
